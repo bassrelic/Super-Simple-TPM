283d26b0f816160f94b5df575db4f3352854b8af
Sebastian <h_p_r@web.de>
2022-10-16 20:13:27 +0200

    Merge SPI slave code into curretn version

--- Super-Simple-TPM/Super-Simple-TPM.ino
+++ Super-Simple-TPM/Super-Simple-TPM.ino
@@ -23,8 +23,24 @@
 #include <Crypto.h>
 #include <AES.h>
 #include <StaticSerialCommands.h>
+#include <SPI.h>
+#include <FastLED.h>
+#include "pins_arduino.h"
 
 #define DATARATE 9600
+#define DATA_PIN 3
+#define COLOR_ORDER GRB
+#define LED_TYPE WS2812
+#define NUM_LEDS 14
+
+#define BRIGHTNESS 150
+#define FRAMES_PER_SECOND 60
+
+CRGB leds[NUM_LEDS];
+
+volatile boolean received;
+volatile int recCount = 0;
+volatile byte receivedData[16];
 
 void cmd_help(SerialCommands &sender, Args &args);
 void cmd_tpm(SerialCommands &sender, Args &args);
@@ -75,6 +91,16 @@ Command commands[]{
 char staticSerialBuffer[256];
 SerialCommands serialCommands(Serial, commands, sizeof(commands) / sizeof(Command), staticSerialBuffer, sizeof(staticSerialBuffer));
 
+ISR(SPI_STC_vect) // Inerrrput routine function
+{
+    if (recCount < 8)
+    {
+        receivedData[recCount] = SPDR; // Get the received data from SPDR register
+        received = true;               // Sets received as True
+        recCount++;
+    }
+}
+
 AES256 aes256;
 byte cypherBuffer[16];
 
@@ -88,25 +114,25 @@ struct AES256Vector
 
 static AES256Vector vectorAES256 = {
     .name = "AES-256-ECB",
-    .key =          {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-                     0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-                     0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-                     0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F},
-    .plaintext =    {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-                     0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF},
-    .ciphertext =   {0x8E, 0xA2, 0xB7, 0xCA, 0x51, 0x67, 0x45, 0xBF,
-                     0xEA, 0xFC, 0x49, 0x90, 0x4B, 0x49, 0x60, 0x89}};
+    .key = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+            0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F},
+    .plaintext = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+                  0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF},
+    .ciphertext = {0x8E, 0xA2, 0xB7, 0xCA, 0x51, 0x67, 0x45, 0xBF,
+                   0xEA, 0xFC, 0x49, 0x90, 0x4B, 0x49, 0x60, 0x89}};
 
 static AES256Vector vectorAES256Crossref = {
     .name = "AES-256-ECB-crossref",
-    .key =          {0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x00,0x01,
-                     0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,
-                     0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x00,0x01,
-                     0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09},
-    .plaintext =    {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,
-                     0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff},
-    .ciphertext =   {0xfe,0x46,0x47,0x0f,0x75,0x46,0x21,0x74,
-                     0x86,0xc3,0x03,0xfb,0x29,0xc2,0xd0,0x8c}};
+    .key = {0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x00, 0x01,
+            0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+            0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x00, 0x01,
+            0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09},
+    .plaintext = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+                  0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff},
+    .ciphertext = {0xfe, 0x46, 0x47, 0x0f, 0x75, 0x46, 0x21, 0x74,
+                   0x86, 0xc3, 0x03, 0xfb, 0x29, 0xc2, 0xd0, 0x8c}};
 
 void testCipher(BlockCipher *cipher, const struct AES256Vector *test)
 {
@@ -156,6 +182,17 @@ void decryptAES256(BlockCipher *cipher, const struct AES256Vector *vector)
 
 void setup()
 {
+    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); // initializes LED strip
+    FastLED.setBrightness(BRIGHTNESS);                                                               // global brightness
+    showProgramGray();
+
+    pinMode(SS, INPUT_PULLUP);
+    pinMode(MOSI, INPUT);
+    pinMode(SCK, INPUT);
+    pinMode(MISO, OUTPUT); // Sets MISO as OUTPUT
+
+    SPCR |= _BV(SPE); // Turn on SPI in Slave Mode
+    SPCR |= _BV(SPIE);
 
     Serial.begin(DATARATE);
     serialCommands.listCommands();
@@ -176,6 +213,34 @@ void setup()
 void loop()
 {
     serialCommands.readSerial();
+
+    if (recCount >= 8)
+    {
+        SPCR &= ~_BV(SPE); // Turn on SPI in Slave Mode
+        recCount = 0;
+        // SPDR = receivedData;    // send back the received data, this is not necessary, only for demo purpose
+        received = false;
+        // Serial.println((char *)receivedData);
+        // Serial.write('\n');
+        if (strcmp(receivedData, "ALAAAARM") == 0)
+        {
+            while (true)
+            {
+                showPolice(250);
+            }
+        }
+        if (strcmp(receivedData, "ALLES_OK") == 0)
+        {
+            for (int i = 0; i < NUM_LEDS; i++)
+            {
+                showProgramRandom();
+                delay(100);
+            }
+            showProgramWhite(); // show "random" program
+        }
+        SPCR |= _BV(SPE); // Turn on SPI in Slave Mode
+        recCount = 0;
+    }
 }
 
 void cmd_help(SerialCommands &sender, Args &args)
@@ -260,9 +325,71 @@ void cmd_tpm_check(SerialCommands &sender, Args &args)
 {
     Serial.println("Check against original Key:");
     testCipher(&aes256, &vectorAES256);
-    for(int i=0; i<32; i++){
-    vectorAES256Crossref.key[i] = vectorAES256.key[i];
+    for (int i = 0; i < 32; i++)
+    {
+        vectorAES256Crossref.key[i] = vectorAES256.key[i];
     }
     Serial.println("Check against testing reference Key:");
     testCipher(&aes256, &vectorAES256Crossref);
+}
+
+void showProgramRandom()
+{
+    for (int i = 0; i < NUM_LEDS; ++i)
+    {
+        leds[i] = CHSV(random8(), 255, 255); // hue, saturation, value
+        FastLED.show();
+    }
+}
+
+void showProgramCleanUp(long delayTime)
+{
+    for (int i = 0; i < NUM_LEDS; ++i)
+    {
+        leds[i] = CRGB::Black;
+    }
+    FastLED.show();
+    delay(delayTime);
+}
+
+void showProgramGray()
+{
+    for (int i = 0; i < NUM_LEDS; ++i)
+    {
+        leds[i] = CRGB::DarkSlateGray;
+    }
+    FastLED.show();
+}
+
+void showProgramWhite()
+{
+    for (int i = 0; i < NUM_LEDS; ++i)
+    {
+        leds[i] = CRGB::White;
+    }
+    FastLED.show();
+}
+
+void showPolice(long delayTime)
+{
+    for (int i = 0; i < NUM_LEDS / 2; ++i)
+    {
+        leds[i] = CRGB::Red;
+    }
+    for (int i = NUM_LEDS; i > NUM_LEDS / 2; --i)
+    {
+        leds[i] = CRGB::Blue;
+    }
+    FastLED.show();
+    delay(delayTime);
+    for (int i = 0; i < NUM_LEDS / 2; ++i)
+    {
+        leds[i] = CRGB::Blue;
+    }
+    for (int i = NUM_LEDS; i > NUM_LEDS / 2; --i)
+    {
+        leds[i] = CRGB::Red;
+    }
+    FastLED.show();
+    delay(delayTime);
 }
